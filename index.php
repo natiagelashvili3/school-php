a0cd43b834191442e19d4f9030595f8c19a351f5
Natia Gelashvili <natia.gelashvili@flatrocktech.com>
2020-11-21 13:46:49 +0400

    filters

--- index.php
+++ index.php
@@ -6,9 +6,15 @@
 
     
     $orderBy = '';
+    $where = [];
+    $whereCondition = '';
 
-    if(!empty($_GET) && isset($_GET['ord'])) {
-        $orderValue = $_GET['ord'];
+    $book_name = '';
+    $release_date = '';
+    $order_parameter = ''; 
+
+    if(!empty($_GET) && isset($_GET['order'])) {
+        $orderValue = $_GET['order'];
         $orderParameters = explode('-', $orderValue);
         
         if(count($orderParameters) == 2) {
@@ -17,16 +23,40 @@
 
             if ( in_array($fieldName, ['id', 'book_name']) && in_array($fieldValue, ['asc', 'desc']) ) {
                 $orderBy = " ORDER BY " . $fieldName . " " . $fieldValue;
+                $order_parameter = $_GET['order'];
             }            
 
         }
 
     }
-    
-    $query = "SELECT * FROM books".$orderBy;
+
+    if (!empty($_GET) && isset($_GET['book_name']) && $_GET['book_name'] != '') {
+        $where[] = "`book_name` LIKE '%".$_GET['book_name']."%'";
+        $book_name = $_GET['book_name'];
+    }
+
+    if (!empty($_GET) && isset($_GET['release_date']) && $_GET['release_date'] != '') {
+        $where[] = "`release_date` = ".$_GET['release_date'];
+        $release_date = $_GET['release_date'];
+    }
+
+    if (!empty($where)) {
+        $whereCondition = implode(" AND ", $where);
+    }
+
+    // if($whereCondition != '') {
+    //     $query = "SELECT *  FROM books WHERE " . $whereCondition;
+    // } else {
+    //     $query = "SELECT *  FROM books ";
+    // }
+
+    // $query .= $orderBy;
+     
+    $query = "SELECT * 
+                FROM books " . ( $whereCondition != '' ? " WHERE ".$whereCondition : '' ) . $orderBy;
+
     $students = $myDatabase->prepare($query);
     $students->execute();
-
 
 ?>
 
@@ -34,11 +64,36 @@
     <div class="container">
         <div class="content">
 
-            <div class="sort">
-                <a href="?ord=id-asc">Order by ID ASC</a>
-                <a href="?ord=id-desc">Order by ID DESC</a>
-                <a href="?ord=book_name-asc">Order by Name ASC</a>
-                <a href="?ord=book_name-desc">Order by Name DESC</a>
+            <div class="filters">
+                <form action="">
+                    <div class="form-group">
+                        <input type="text" 
+                            placeholder="Book Name" 
+                            name="book_name" 
+                            value="<?= $book_name ?>">
+                    </div>
+                    <div class="form-group">
+                        <select name="release_date" id="">
+                            <option value="" <?= $release_date == '' ? 'selected' : '' ?>>Select Year</option>
+                            <?php for ($i=2018; $i < 2023; $i++) { 
+                                ?>
+                                    <option value="<?= $i ?>" <?= $release_date == $i ? 'selected' : '' ?>><?= $i ?></option>
+                                <?php
+                            }?>
+                        
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <select name="order" id="">
+                            <option value="" <?= $order_parameter == '' ? 'selected' : '' ?> >Select Order</option>
+                            <option value="id-asc" <?= $order_parameter == 'id-asc' ? 'selected' : '' ?>>Order by ID ASC</option>
+                            <option value="id-desc" <?= $order_parameter == 'id-desc' ? 'selected' : '' ?>>Order by ID DESC</option>
+                            <option value="book_name-asc" <?= $order_parameter == 'book_name-asc' ? 'selected' : '' ?>>Order by Name ASC</option>
+                            <option value="book_name-desc" <?= $order_parameter == 'book_name-desc' ? 'selected' : '' ?>>Order by Name DESC</option>
+                        </select>
+                    </div>
+                    <button type="submit">Search</button>
+                </form>
             </div>
 
             <table id="book-list">
